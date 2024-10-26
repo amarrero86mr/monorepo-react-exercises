e6da04125778f7f4f5d1c2903687ae103724b42d
uriel <urielaraujo@live.com.ar>
2024-10-25 23:51:49 -0300

    get-random-color/868af4gqa introducción a useState y hooks

--- get-random-color/src/App.tsx
+++ get-random-color/src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // const estadoDinero = useState(100);
+
+  // const dinero = estadoDinero[0];
+  // const modificarDinero = estadoDinero[1];
+
+  const [dinero, setDinero] = useState(0);
+
+  console.log("el total al renderizar es: " + dinero);
   return (
     <>
       <h1>get random color</h1>
+      <p>{dinero} usd</p>
+      <button
+        onClick={() => {
+          console.log("se envían 10 usd!");
+          setDinero(dinero - 10);
+          console.log("el total actual es: " + dinero);
+        }}
+      >
+        Enviar 10 usd
+      </button>
     </>
   );
 }

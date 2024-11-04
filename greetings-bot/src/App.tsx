9e71a479048effebf7ef4415a7006f2589e08e93
amarrero86mr <amarrero86mr@gmail.com>
2024-11-04 19:10:45 -0300

    greetings-bot/868ajgy4d/agustin agregando boton y metodo de saludo por consola

--- greetings-bot/src/App.tsx
+++ greetings-bot/src/App.tsx
@@ -4,6 +4,18 @@ function App() {
   return (
     <>
       <h1 className="text-center text-3xl">greetings bot</h1>
+
+      <div className="flex justify-center m-10">
+          {/* Implementacion de Boton Click me!  */}
+        <button 
+        className="bg-teal-500 w-20 h-8 border-2 text-blue-950 border-teal-900 font-medium"
+        // Uso de onClick para escuchar el evento click
+        onClick={()=>{
+          // Uso de Console.log() para imprimir el mensaje: Hola!
+          console.log("Hola!")
+          }}
+        >Click me!</button>
+      </div>
     </>
   );
 }
